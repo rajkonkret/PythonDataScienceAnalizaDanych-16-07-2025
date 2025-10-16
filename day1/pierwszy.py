da85438ca3be76d9c757a118f3a68ba7ba935b5f
rajkonkret <rajkonkret660@gmail.com>
2025-10-16 12:30:18 +0200

    [1 3] update pierwszy.py

--- day1/pierwszy.py
+++ day1/pierwszy.py
@@ -96,3 +96,37 @@ print(bool(""))  # False
 
 # None - nie wiem, stan nieokreslony, odpowiednik null
 print(bool(None))  # False
+
+temp = 36.6
+print(type(temp))  # <class 'float'>, zmiennoprzecinkowe
+print(temp)  # 36.6
+print(sys.float_info)
+# sys.float_info(max=1.7976931348623157e+308, max_exp=1024,
+# max_10_exp=308, min=2.2250738585072014e-308, min_exp=-1021,
+# min_10_exp=-307, dig=15, mant_dig=53, epsilon=2.220446049250313e-16, radix=2, rounds=1)
+
+# float - bład zaokaglenia
+print(0.1 + 0.9)  # 1.0
+print(0.1 + 0.7)  # 0.7999999999999999
+print(0.1 + 0.2)  # 0.30000000000000004
+#  For example, in a floating-point arithmetic with five base-ten digits,
+#  the sum 12.345 + 1.0001 = 13.3451 might be rounded to 13.345.
+# decimal - ominięcie problemu zaokrąglenia
+
+# f - string format
+print(f"Nazywam się {name}, Dzień dobry")
+# Nazywam się 90, Dzień dobry
+print("Nazywam się {}.".format(name))  # Nazywam się 90.
+
+liczba = 3.900001
+print(f"Wersja pythona: {liczba}")  # Wersja pythona: 3.900001
+print(f"Wersja pythona: {liczba:.2f}")  # Wersja pythona: 3.90 - zaokrągla
+
+print(f"""Tekst
+    wielolinijkowy""")
+# "Tekst
+#     wielolinijkowy"
+
+"""Komentarz
+    wielolinijkowy - dokumentacja"""
+print(print.__doc__)
