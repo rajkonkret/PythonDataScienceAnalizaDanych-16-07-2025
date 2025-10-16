e1c9055a1bb9dafca950260eff2159ac05be022b
rajkonkret <rajkonkret660@gmail.com>
2025-10-16 15:04:58 +0200

    [1 14]add zad_hello.py

---
+++ day1/hello.py
@@ -0,0 +1,4 @@
+x = 2345
+y = 678
+imie = "Radek"
+nazwisko = "Kowalski"
---
+++ day1/zad_hello.py
@@ -0,0 +1,12 @@
+import hello
+
+print(hello.x)
+print(hello.y)
+
+# 2345
+# 678
+
+print(hello.imie)
+print(hello.nazwisko)
+# Radek
+# Kowalski
