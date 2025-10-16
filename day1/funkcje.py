be9883974cafa28a8853d435746a82b1da3fafbd
rajkonkret <rajkonkret660@gmail.com>
2025-10-16 14:55:43 +0200

    [1 12] update funkcje.py

--- day1/funkcje.py
+++ day1/funkcje.py
@@ -27,3 +27,43 @@ print(ksero(5, 5))  # "5" * 5, 55555
 wynik = ksero("Radek", 5)
 print(wynik)  # RadekRadekRadekRadekRadek
 
+
+def ksero2(tekst: str, ile: int = 1):
+    """
+    Ta funkcja zwraca string pomnożony ilosc razy
+    :param tekst:
+    :param ile:
+    :return:
+    """
+    return str(tekst) * ile  # zwraca wynik
+
+
+print(ksero2("Radek"))  # Radek
+print(ksero2("Radek", 2))  # RadekRadek
+
+
+def ksero3(tekst: str = "tresc", ile: int = 1):
+    """
+    Ta funkcja zwraca string pomnożony ilosc razy
+    :param tekst:
+    :param ile:
+    :return:
+    """
+    return str(tekst) * ile  # zwraca wynik
+
+
+print(ksero3())  # tresc
+print(ksero3(tekst="Radek", ile=3))  # RadekRadekRadek
+
+
+def ile_razy(*ile, **co):
+    print(ile)
+    print(co)
+
+
+# * argumenty pozycyjne
+# ** argumentów nazwane, słownikowe
+ile_razy((4, 5, 6))  # ((4, 5, 6),) -> ile
+ile_razy(4, 5, 6, name="Radek", status="OK")  # ((4, 5, 6),) -> ile
+# (4, 5, 6)
+# {'name': 'Radek', 'status': 'OK'}
